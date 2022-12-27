afedbd87be806a42d6bd431b6f655b0431d82941
sgn07124 <76249685+sgn07124@users.noreply.github.com>
2022-12-27 17:39:34 +0900

    [Bronze I] Title: Yangjojang of The Year, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/11557. Yangjojang of The Year/Yangjojang of The Year.py
@@ -0,0 +1,13 @@
+t = int(input())
+
+for i in range(t) :
+    n = int(input())
+    max = 0
+    maxname = ''
+    for j in range(n) :
+        name, liter = input().split()  # name : 학교 이름, liter : 술의 양
+        liter = int(liter)  # 술의 양을 int형으로 변경
+        if liter > max :   
+            max = liter
+            maxname = name
+    print(maxname)
