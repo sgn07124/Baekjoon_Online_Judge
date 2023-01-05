5c04717d9335fb1de5f4a9367732c9abcfd6790e
Kim Kyeongho <76249685+sgn07124@users.noreply.github.com>
2023-01-05 18:17:31 +0900

    Update AC.py

--- 백준/Gold/5430. AC/AC.py
+++ 백준/Gold/5430. AC/AC.py
@@ -16,14 +16,14 @@ for i in range(t) :
         back = 0
     for j in p :
         if j == "R" :
-            rev += 1
+            rev += 1  # Reverse를 해야 할 때마다, 매번 deque를 뒤집어주면 시간초과가 발생하므로 Reverse를 실행하지 않고, 뒤집는 횟수를 기억해둔다.
         elif j == "D" :
             if len(queue) < 1 :
                 flag = 1
                 print("error")
                 break
             else :
-                if rev % 2 == 0 :
+                if rev % 2 == 0 :  # 뒤집는 횟수가 짝수이므로 popleft
                     queue.popleft()
                 else :
                     queue.pop()
@@ -31,5 +31,5 @@ for i in range(t) :
         if rev % 2 == 0 :
             print("[" + ",".join(queue) + "]")
         else :
-            queue.reverse()
+            queue.reverse()  # Reverse(뒤집는 횟수)가 홀수 번일 때, 뒤집는다.
             print("[" + ",".join(queue) + "]")
