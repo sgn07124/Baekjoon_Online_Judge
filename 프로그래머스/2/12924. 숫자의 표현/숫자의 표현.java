716bb92e6962dbf48665b8c65c1e548e54254991
Kim Kyungho <76249685+sgn07124@users.noreply.github.com>
2024-11-15 18:35:02 +0900

    [level 2] Title: 숫자의 표현, Time: 1.73 ms, Memory: 52 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
@@ -1,26 +1,6 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        
-        for (int i = 1; i <= n; i++) {
-            if (isSum(n, i) == true) {
-                answer++;
-            }
-        }
-        
         return answer;
     }
-    
-    public static boolean isSum(int n, int i) {
-        int sum = 0;
-        for (int j = i; j <= n; j++) {
-            sum += j;
-            if (sum == n) {
-                return true;
-            } else if (sum > n) {
-                break;
-            } 
-        }
-        return false;
-    }
- }
+}
