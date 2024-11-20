8eba2199c5a3a6aff962f99528049d565a57b863
Kim Kyungho <76249685+sgn07124@users.noreply.github.com>
2024-11-20 16:06:12 +0900

    [level 2] Title: 올바른 괄호, Time: 6.89 ms, Memory: 68.7 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -7,12 +7,11 @@ class Solution {
             if (c == '(') {
                 cnt++;
             } else if (c == ')') cnt--;
-            if (cnt < 0) return false;
+            
+            if (cnt < 0) return false;  // cnt 값이 음수가 되면 괄호가 제대로 짝지어지지 않은 것
         }
         if (cnt == 0) answer = true;
         
-        
-
         return answer;
     }
 }
