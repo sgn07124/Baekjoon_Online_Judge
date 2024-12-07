e5ea67986c9b83a0f5db3652d713788ab4c13698
Kim Kyungho <76249685+sgn07124@users.noreply.github.com>
2024-12-08 01:56:15 +0900

    [Silver II] Title: Generic Queries, Time: 1220 ms, Memory: 307544 KB -BaekjoonHub

---
+++ 백준/Silver/16713. Generic Queries/Generic Queries.java
@@ -0,0 +1,42 @@
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int Q = Integer.parseInt(st.nextToken());
+
+        int [] arr = new int[N + 1];
+        int [] acc = new int[N + 1];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 1; i <= N; i++) {
+            acc[i] = acc[i - 1] ^ arr[i];
+        }
+
+        int ans = 0;
+        for (int i = 0; i < Q; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            ans ^= acc[b] ^ acc[a - 1];
+        }
+        bw.write(ans + "");
+        bw.flush();
+        bw.close();
+    }
+}
