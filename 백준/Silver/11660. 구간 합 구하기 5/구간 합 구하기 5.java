b781caa561ec962bccfd2990c3c1beca17252901
Kim Kyungho <76249685+sgn07124@users.noreply.github.com>
2024-12-08 21:51:00 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 792 ms, Memory: 125220 KB -BaekjoonHub

---
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());  // 표의 크기
+        int M = Integer.parseInt(st.nextToken());  // 합을 구해야 하는 횟수
+
+        int [][] arr = new int[N + 1][N + 1];
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= N; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        int [][] acc = new int[N + 1][N + 1];
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                acc[i][j] = acc[i - 1][j] + acc[i][j - 1] - acc[i - 1][j - 1] + arr[i][j];
+            }
+        }
+
+        int ans = 0;
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+            ans = acc[x2][y2] - acc[x1 - 1][y2] - acc[x2][y1 - 1] + acc[x1 - 1][y1 - 1];
+            bw.write(ans + "\n");
+        }
+        bw.flush();
+        bw.close();
+    }
+}
