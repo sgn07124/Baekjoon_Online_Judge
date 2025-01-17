50ecdf2fbc55c27fa1713005e65d3382edc860f9
Kim Kyungho <76249685+sgn07124@users.noreply.github.com>
2025-01-17 16:45:25 +0900

    [Gold V] Title: 토마토, Time: 628 ms, Memory: 106100 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.java
+++ 백준/Gold/7569. 토마토/토마토.java
@@ -1,81 +1,74 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
+import java.util.ArrayDeque;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-
-    static int[][][] map;  // 게임판
-    static boolean[][][] visited;  // 방문처리
-    static int[] dx = {1, -1, 0, 0, 0, 0};
-    static int[] dy = {0, 0, 1, -1, 0, 0};
-    static int[] dz = {0, 0, 0, 0, 1, -1};
-    static int m, n, h;
-    static int days = 0;  // 일수
-    static int yet = 0;  // 익지 않은 토마토의 개수
-
+    static int M, N, H;
+    static int yet = 0, days = 0;
+    static int [][][] storage;
+    static boolean [][][] visited;
+    static int [] dx = {1, -1, 0, 0, 0, 0};
+    static int [] dy = {0, 0, -1, 1, 0, 0};
+    static int [] dz = {0, 0, 0, 0, 1, -1};
+    // 전체적으로 일수를 계산하는 것이므로 Queue에 토마토를 입력받으면서 넣야야 한다!!
+    static Queue<int []> queue = new ArrayDeque<>();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        m = Integer.parseInt(st.nextToken());
-        n = Integer.parseInt(st.nextToken());
-        h = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());  // 가로
+        N = Integer.parseInt(st.nextToken());  // 세로
+        H = Integer.parseInt(st.nextToken());  // 높이
 
-        map = new int[h][n][m];
-        visited = new boolean[h][n][m];
-        Queue<int[]> queue = new LinkedList<>();
-
-        for (int i = 0; i < h; i++) {
-            for (int j = 0; j < n; j++) {
+        storage = new int[H][N][M];
+        visited = new boolean[H][N][M];
+        for (int i = 0; i < H; i++) {
+            for (int j = 0; j < N; j++) {
                 st = new StringTokenizer(br.readLine());
-                for (int k = 0; k < m; k++) {
-                    int tomato = Integer.parseInt(st.nextToken());
-                    map[i][j][k] = tomato;
-                    if (tomato == 1) {  // 토마토가 있는 부분(1)은 큐에 추가하고 
-                        queue.add(new int[] {j, k, i, 0});
-                        visited[i][j][k] = true;  // 익은 토마토는 다시 방문할 필요가 없으니 true로 방문 처리
-                    } else if (tomato == 0) {
-                        yet++;  // 익지 않은 토마토(0)의 경우, yet을 증가
+                for (int l = 0; l < M; l++) {
+                    storage[i][j][l] = Integer.parseInt(st.nextToken());
+                    if (storage[i][j][l] == 1){
+                        queue.add(new int[] {j, l, i, 0});  // x, y, h, day
+                        visited[i][j][l] = true;  // 익은 토마토는 방문할 필요가 없으므로 방문 처리
+                    } else if (storage[i][j][l] == 0) {
+                        yet++;  // 익지 않은 토마토의 개수
                     }
                 }
             }
         }
-        if (yet == 0) {  // 모든 토마토가 다 익은 경우
-            System.out.println(0);
+
+        if (yet == 0) {
+            System.out.println(days);
             return;
         }
-        bfs(visited, queue);
+        bfs();
         if (yet != 0) {
             System.out.println(-1);
             return;
         }
         System.out.println(days);
     }
-
-    private static void bfs(boolean[][][] visited, Queue<int[]> queue) {
-        int nextX, nextY, nextZ;
-
-        while(!queue.isEmpty()) {
-            int[] pos = queue.poll();
+    static void bfs() {
+        while (!queue.isEmpty()) {
+            int [] pos = queue.poll();
 
             for (int i = 0; i < 6; i++) {
-                nextX = pos[0] + dx[i];
-                nextY = pos[1] + dy[i];
-                nextZ = pos[2] + dz[i];
+                int nx = pos[0] + dx[i];
+                int ny = pos[1] + dy[i];
+                int nz = pos[2] + dz[i];
 
-                if (nextX < 0 || nextX > n-1 || nextY < 0 || nextY > m-1 || nextZ < 0 || nextZ > h-1 ||
-                map[nextZ][nextX][nextY] != 0 || visited[nextZ][nextX][nextY]) {
-                    continue;   // 무시
+                if (nx >= 0 && nx < N && ny >= 0 && ny < M && nz >= 0 && nz < H) {
+                    if (!visited[nz][nx][ny] && storage[nz][nx][ny] == 0) {
+                        storage[nz][nx][ny] = 1;  // 익은 토마토 처리
+                        visited[nz][nx][ny] = true;  // 방문 처리
+                        yet--;
+                        days = Math.max(days, pos[3] + 1);
+                        queue.add(new int[] {nx, ny, nz, pos[3] + 1});
+                    }
                 }
-
-                map[nextZ][nextX][nextY] = 1;
-                visited[nextZ][nextX][nextY] = true;
-                yet--;
-                days = Math.max(days, pos[3] + 1);
-                queue.add(new int[] {nextX, nextY, nextZ, pos[3] + 1});
             }
         }
     }
