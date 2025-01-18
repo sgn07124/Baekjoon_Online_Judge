f628d983fd160abf8c00dc7598370301a99d50f9
Kim Kyungho <76249685+sgn07124@users.noreply.github.com>
2025-01-18 15:24:42 +0900

    [Silver I] Title: 나이트의 이동, Time: 292 ms, Memory: 71040 KB -BaekjoonHub

--- 백준/Silver/7562. 나이트의 이동/나이트의 이동.java
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.java
@@ -1,66 +1,57 @@
-
 import java.io.*;
-import java.util.ArrayDeque;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-    static int n;  // 체스판 크기
-    static int[][] map;  // 게임판
-    static boolean[][] visited;  // 방문
+    static int [][] map;
+    static boolean [][] visited;
+    static int count = 0;
+    static int l, nowX, nowY, desX, desY;
+    static Queue<int []> queue = new ArrayDeque<>();
     static int [] dx = {-2, -1, 1, 2, 2, 1, -1, -2};
     static int [] dy = {1, 2, 2, 1, -1, -2, -2, -1};
-    static int nowX, nowY;  // 현재 위치 칸
-    static int desX, desY;  // 이동하려는 칸
-
-    // bfs로 최소 이동 횟수 계산
-    static void bfs(int x, int y) {
-        ArrayDeque<int[]> queue = new ArrayDeque<>();
-        queue.add(new int[] {x, y});  // 시작 지점을 큐에 추가
-        map[x][y] = 0;  // 시작 지점은 이동 횟수가 0
-
-        while (!queue.isEmpty()) {
-            int[] now = queue.poll();  // 큐에서 현재 위치를 꺼냄
-            int nowX = now[0];
-            int nowY = now[1];
-
-            // 나이트의 8가지 이동 방향을 모두 확인
-            for (int i = 0; i < 8; i++) {
-                int nextX = nowX + dx[i];
-                int nextY = nowY + dy[i];
-
-                // 체스판을 벗어나지 않고, 방문하지 않은 위치인 경우
-                if (nextX >= 0 && nextX < n && nextY >= 0 && nextY < n && !visited[nextX][nextY]) {
-                    visited[nextX][nextY] = true;  // 방문 처리
-                    map[nextX][nextY] = map[nowX][nowY] + 1;  // 이동 횟수를 1 증가시켜 저장
-                    queue.add(new int[]{nextX, nextY});  // 다음 위치를 큐에 추가
-                }
-            }
-        }
-    }
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
 
         int T = Integer.parseInt(br.readLine());
 
-        for (int i = 0; i < T; i++) {
-            n = Integer.parseInt(br.readLine());  // 게임판 한 변 칸 수
-            map = new int[n][n];
-            visited = new boolean[n][n];
+        while (T-- > 0) {
+            l = Integer.parseInt(br.readLine());  // 체스판의 한 변의 길이
+            map = new int[l][l];
+            visited = new boolean[l][l];
             st = new StringTokenizer(br.readLine());
+            // 나이트의 위치
             nowX = Integer.parseInt(st.nextToken());
             nowY = Integer.parseInt(st.nextToken());
             st = new StringTokenizer(br.readLine());
+            // 나이트가 이동하려고 하는 칸
             desX = Integer.parseInt(st.nextToken());
             desY = Integer.parseInt(st.nextToken());
-
-            visited[nowX][nowY] = true;  // 시작 지점을 방문 처리
             bfs(nowX, nowY);
-            bw.write(map[desX][desY] + "\n");
+            sb.append(map[desX][desY]).append('\n');  // 해당 위치의 가중치가 이동 횟수
         }
-        bw.flush();
-        bw.close();
+        System.out.println(sb);
+    }
+
+    static void bfs(int x, int y) {
+        queue = new ArrayDeque<>();
+        queue.add(new int[] {x, y});
+        visited[x][y] = true;
+
+        while (!queue.isEmpty()) {
+            int [] now = queue.poll();
+
+            for (int i = 0; i < 8; i++) {
+                int nx = now[0] + dx[i];
+                int ny = now[1] + dy[i];
+
+                if (nx >= 0 && nx < l && ny >= 0 && ny < l && !visited[nx][ny]) {
+                    visited[nx][ny] = true;
+                    map[nx][ny] = map[now[0]][now[1]] + 1;  // 이동 횟수를 1만큼 증가시켜 저장
+                    queue.add(new int[] {nx, ny});
+                }
+            }
+        }
     }
 }
