9cda18b22e0258be70315e0dbbbc251addc8c802
Kim Kyungho <76249685+sgn07124@users.noreply.github.com>
2025-01-20 23:38:35 +0900

    [Silver II] Title: 이항 계수 2, Time: 112 ms, Memory: 17932 KB -BaekjoonHub

---
+++ 백준/Silver/11051. 이항 계수 2/이항 계수 2.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int [][] dp;
+    static final int div = 10007;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        dp = new int[N + 1][K + 1];
+        System.out.println(BC(N, K));
+
+    }
+
+    static int BC(int n, int k) {
+        if (dp[n][k] > 0) {
+            return dp[n][k];
+        }
+        
+        if (k == 0 || n == k) {
+            return dp[n][k] = 1;
+        }
+        return dp[n][k] = (BC(n - 1, k - 1) + BC(n - 1, k)) % div;
+    }
+}
