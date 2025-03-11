242ddb755c8bd662237c1c101fed00a240b9a9a9
Kim Kyungho <76249685+sgn07124@users.noreply.github.com>
2025-03-12 01:17:00 +0900

    [Silver III] Title: 파도반 수열, Time: 104 ms, Memory: 14120 KB -BaekjoonHub

---
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        long [] dp = new long[101];  // 오버플로우 발생 가능성 있으므로 long형
+        dp[1] = 1;
+        dp[2] = 1;
+        dp[3] = 1;
+        dp[4] = 2;
+        dp[5] = 2;
+
+        for (int i = 6; i <= 100; i++) {
+            dp[i] = dp[i - 1] + dp[i - 5];
+        }
+        for (int i = 0; i < T; i++) {
+            System.out.println(dp[Integer.parseInt(br.readLine())]);
+        }
+    }
+}
