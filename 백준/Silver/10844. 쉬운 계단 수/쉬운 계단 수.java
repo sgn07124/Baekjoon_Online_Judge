b5da58018f28f89cb5da132d0569e5ab97fb47e1
Kim Kyungho <76249685+sgn07124@users.noreply.github.com>
2025-03-12 23:21:53 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 112 ms, Memory: 14428 KB -BaekjoonHub

---
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.java
@@ -0,0 +1,33 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        long [][] dp = new long[n + 1][10];
+
+        // n == 1 의 경우의 수는 1개
+        for (int i = 1; i <= 9; i++) {
+            dp[1][i] = 1;
+        }
+
+        // n > 1
+        for (int i = 2; i <= n; i++) {
+            for (int j = 0; j < 10; j++) {
+                if (j == 0) {
+                    dp[i][j] = dp[i - 1][1] % 1000000000;
+                } else if (j == 9) {
+                    dp[i][j] = dp[i - 1][8] % 1000000000;
+                } else {
+                    dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j + 1]) % 1000000000;
+                }
+            }
+        }
+        System.out.println(Arrays.stream(dp[n]).sum() % 1000000000);
+    }
+}
