6ec2b6a6b83ff0f664e62d08d89c1bfa7a69531d
Kim Kyungho <76249685+sgn07124@users.noreply.github.com>
2025-04-05 19:42:35 +0900

    [Silver I] Title: 동물원, Time: 108 ms, Memory: 14968 KB -BaekjoonHub

---
+++ 백준/Silver/1309. 동물원/동물원.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        long [] dp = new long[n + 1];
+
+        dp[0] = 1;
+        dp[1] = 3;
+        if (n >= 2) {
+            for (int i = 2; i <= n; i++) {
+                dp[i] = (dp[i - 2] + dp[i - 1] * 2) % 9901;
+            }
+        }
+        System.out.println(dp[n]);
+    }
+}
