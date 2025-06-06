8c584b510d58be832c064fa74a7678d2fbe69c33
Kim Kyungho <76249685+sgn07124@users.noreply.github.com>
2025-06-06 17:47:45 +0900

    [Gold IV] Title: 불, Time: 668 ms, Memory: 125264 KB -BaekjoonHub

--- 백준/Gold/5427. 불/불.java
+++ 백준/Gold/5427. 불/불.java
@@ -1,97 +1,91 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
+    static int w, h;
     static int [][] map;
+    static int [] dx = {-1, 1, 0, 0};
+    static int [] dy = {0, 0, 1, -1};
     static Queue<int[]> person;
     static Queue<int[]> fire;
-    static int[] dx = {-1,1,0,0};
-    static int[] dy = {0,0,-1,1};
-    static int w, h;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
         StringBuilder sb = new StringBuilder();
-
+        StringTokenizer st;
         int T = Integer.parseInt(br.readLine());
-
+        
         while (T-- > 0) {
             st = new StringTokenizer(br.readLine());
-            w = Integer.parseInt(st.nextToken());
-            h = Integer.parseInt(st.nextToken());
+            w = Integer.parseInt(st.nextToken());  // 너비
+            h = Integer.parseInt(st.nextToken());  // 높이
+            
             map = new int[h][w];
             person = new LinkedList<>();
             fire = new LinkedList<>();
-
+            
             for (int i = 0; i < h; i++) {
                 String line = br.readLine();
                 for (int j = 0; j < w; j++) {
                     map[i][j] = line.charAt(j);
-                    if (map[i][j] == '@') {
-                        person.add(new int[] {j, i, 0});
-                    } else if (map[i][j] == '*') {
-                        fire.add(new int[] {j, i});
+                    if (line.charAt(j) == '@') {  // 상근이의 위치
+                        person.offer(new int[] {j, i, 0});
+                    } else if (line.charAt(j) == '*') {  // 불의 위치
+                        fire.offer(new int[] {j, i});
                     }
                 }
             }
-            int res = -1;
+            
+            int result = -1;
+            
             out : while (true) {
                 // 불 번짐
-                int fSize = fire.size();
-                for (int i = 0; i < fSize; i++) {
+                int fire_size = fire.size();
+                for (int i = 0; i < fire_size; i++) {
                     int [] pos = fire.poll();
-                    int px = pos[0];
-                    int py = pos[1];
-                    fireMarking(px, py);
+                    fireMarking(pos[0], pos[1]);
                 }
-
+                
                 // 상근 이동
-                int pSize = person.size();
-                for (int i = 0; i < pSize; i++) {
+                int person_size = person.size();
+                for (int i = 0; i < person_size; i++) {
                     int [] pos = person.poll();
-                    int px = pos[0];
-                    int py = pos[1];
-                    int time = pos[2];
-                    res = escape(px, py, time);
-                    if (res != -1) break out;
+                    result = escape(pos[0], pos[1], pos[2]);
+                    if (result != -1) break out;
                 }
-                if (person.isEmpty()) break;  // 상근이가 탈출한 경우
+                if (person.isEmpty()) break;  // 상근이 탈출한 경우
             }
-            if (res != -1) sb.append(res).append('\n');
+            if (result != -1) sb.append(result).append('\n');
             else sb.append("IMPOSSIBLE\n");
         }
         System.out.println(sb);
     }
-
+    
     static void fireMarking(int x, int y) {
         for (int i = 0; i < 4; i++) {
             int nx = x + dx[i];
             int ny = y + dy[i];
-
-            if (nx >= 0 && nx < w && ny >= 0 && ny < h) {
+            
+            if (nx >= 0 && ny >= 0 && nx < w && ny < h) {
                 if (map[ny][nx] == '.' || map[ny][nx] == '@') {
                     map[ny][nx] = '*';
-                    fire.add(new int[] {nx, ny});
+                    fire.offer(new int[] {nx, ny});
                 }
             }
         }
     }
-
+    
     static int escape(int x, int y, int time) {
         for (int i = 0; i < 4; i++) {
             int nx = x + dx[i];
             int ny = y + dy[i];
-
-            if (nx < 0 || nx > w - 1 || ny < 0 || ny > h - 1) {
+            
+            if (nx < 0 || ny < 0 || nx >= w || ny >= h) {
                 return time + 1;
             }
+            
             if (map[ny][nx] == '.') {
                 map[ny][nx] = '@';
-                person.add(new int[] {nx, ny, time + 1});
+                person.offer(new int[] {nx, ny, time + 1});
             }
         }
         return -1;
