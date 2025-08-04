616618e399dad08050736ae3335ba0fc20d8f632
Kim Kyungho <76249685+sgn07124@users.noreply.github.com>
2025-08-04 12:20:23 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 124 ms, Memory: 14552 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -1,35 +1,30 @@
 import java.util.*;
 import java.io.*;
 
-public class Main {
+class Main {
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
-
-        int N = Integer.parseInt(br.readLine());
-        int arr[] = new int[N+1];  // 수열
-        int dp[] = new int[N+1];  // dp
-
+        
+        int n = Integer.parseInt(br.readLine());
+        
+        int [] arr = new int [n + 1];
+        int [] dp = new int [n + 1];
+        int max = 0;
+        
         st = new StringTokenizer(br.readLine());
-        for (int i = 1; i <= N; i++) {
+        for (int i = 1; i <= n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
-            dp[i] = 1;  // 일단 dp값들은 최소 1
+            dp[i] = 1;
         }
-        int max = 1;
-        for (int i = 1; i <= N; i++) {
+        
+        for (int i = 1; i <= n; i++) {
             for (int j = 1; j < i; j++) {
-                if (arr[i] > arr[j]) {  // arr의 현재값이 이전 값보다 크면
-                    // 각 i에 대해 가능한 모든 j를 검사하고, 그 중 가장 긴 증가하는 부분 수열의 길이를 dp[i]에 다시 저장하므로
-                    // max()로 dp[i]를 계속 비교함
-                    dp[i] = Math.max(dp[i], dp[j] + 1);
-                }
+                // 이전의 값보다 크다면? 이전의 원소들 중 가장 큰 값 + 1
+                if (arr[i] > arr[j]) dp[i] = Math.max(dp[i], dp[j] + 1);
             }
-            max = Math.max(max, dp[i]);  // 최댓값 저장
+            max = Math.max(dp[i], max);
         }
-        bw.write(max + "");
-        bw.flush();
-        bw.close();
+        System.out.println(max);
     }
 }
